b63c0eff81332ae4b1c3d91a01490fe39b6272d2
alimomtaznoor <noorali.momtaz98@gmail.com>
2025-06-02 23:35:29 +0530

    deployment issue fixes

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,54 +1,18 @@
-import { EmptyState, Pagination, SharedHeader, VideoCard } from "@/components";
-import { getAllVideos } from "@/lib/actions/video";
+"use client";
+import { redirect } from "next/navigation";
+import Hero from "@/components/Hero";
 
-const page = async ({ searchParams }: SearchParams) => {
-  const { query, filter, page } = await searchParams;
+import { createAuthClient } from "better-auth/react";
 
-  const { videos, pagination } = await getAllVideos(
-    query,
-    filter,
-    Number(page) || 1
-  );
+const { useSession } = createAuthClient();
 
-  return (
-    <main className="wrapper page">
-      <SharedHeader subHeader="Public Library" title="All Videos" />
+const HomePage = () => {
+  const { data: session } = useSession();
+  if (session?.user) {
+    redirect("/media");
+  }
 
-      {videos?.length > 0 ? (
-        <section className="video-grid">
-          {videos.map(({ video, user }) => (
-            <VideoCard
-              key={video.id}
-              id={video.videoId}
-              title={video.title}
-              thumbnail={video.thumbnailUrl}
-              createdAt={video.createdAt}
-              userImg={user?.image ?? ""}
-              username={user?.name ?? "Guest"}
-              views={video.views}
-              visibility={video.visibility}
-              duration={video.duration}
-            />
-          ))}
-        </section>
-      ) : (
-        <EmptyState
-          icon="/assets/icons/video.svg"
-          title="No Videos Found"
-          description="Try adjusting your search."
-        />
-      )}
-
-      {pagination?.totalPages > 1 && (
-        <Pagination
-          currentPage={pagination.currentPage}
-          totalPages={pagination.totalPages}
-          queryString={query}
-          filterString={filter}
-        />
-      )}
-    </main>
-  );
+  return <Hero />;
 };
 
-export default page;
+export default HomePage;
