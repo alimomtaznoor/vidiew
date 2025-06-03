e100e6588bb6135ef49191fb25b34cf16984a5b9
alimomtaznoor <noorali.momtaz98@gmail.com>
2025-06-03 17:09:53 +0530

    session expiration imp

--- middleware.ts
+++ middleware.ts
@@ -13,7 +13,7 @@ export async function middleware(request: NextRequest) {
   // }
 
   if (!session) {
-    return NextResponse.redirect(new URL("/" , request.url));
+    return NextResponse.redirect(new URL("/sign-in" , request.url));
   }
 
 
